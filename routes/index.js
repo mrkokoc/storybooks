aa339c7b56480a875dd35ac0ecfb163317ba872d
mrkokoc <ilia.guskov@gmail.com>
2017-10-19 13:43:29 +0300

    UI improvements

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const {ensureAuthenticated} = require('../helpers/auth');
 
 router.get('/', (req, res) => {
     res.render('index/welcome');
 });
 
-router.get('/dashboard', (req, res) => {
+router.get('/dashboard', ensureAuthenticated, (req, res) => {
     res.render('index/dashboard');
+});
+
+router.get('/about', (req, res) => {
+    res.render('index/about');
 });
 
 module.exports = router;
